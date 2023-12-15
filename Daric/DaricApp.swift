7aac9806ede43ba3165c93176848b47e48ed1a5a
Hamed Mousavi <me@hamedmousavi.com>
2023-12-15 23:58:30 +0000

    Initial Commit

---
+++ Daric/DaricApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaricApp.swift
+//  Daric
+//
+//  Created by Hamed Mousavi on 15/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct DaricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DaricUITests/DaricUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DaricUITestsLaunchTests.swift
+//  DaricUITests
+//
+//  Created by Hamed Mousavi on 15/12/2023.
+//
+
+import XCTest
+
+final class DaricUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
