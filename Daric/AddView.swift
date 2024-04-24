c0846156371cbcc0d7ae3b17069001b70275900c
Hamed Mousavi <me@hamedmousavi.com>
2024-04-24 16:45:22 +0100

    Publish

---
+++ Daric/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  Daric
+//
+//  Created by Hamed Mousavi on 11/01/2024.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
---
+++ Daric/NewChallengeVideoView.swift
@@ -0,0 +1,18 @@
+//
+//  NewChallengeVideoView.swift
+//  Daric
+//
+//  Created by Hamed Mousavi on 11/01/2024.
+//
+
+import SwiftUI
+
+struct NewChallengeVideoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewChallengeVideoView()
+}
